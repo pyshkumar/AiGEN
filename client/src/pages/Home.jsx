32a1f36b947331f1606730fac3ab307b865bc785
Piyush Kumar <kpiyush39@gmail.com>
2023-03-18 05:42:34 +0530

    Home page complete onto post page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Loader, Card, FormField } from "../components";
 
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post.id} {...post} />);
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+  );
+}; //{...post} -> spredaing out other post property
+
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
@@ -29,12 +39,18 @@ const Home = () => {
             <>
               {searchText && (
                 <h2 className="font-medium text-[#666e75] text-xl mb">
-                  Searching result{" "}
+                  Showing results for
                   <span className=" text-[#222328]">{searchText}</span>
                 </h2>
               )}
 
-              <div className=""></div>
+              <div className="grid lg:grif-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+                {searchText ? (
+                  <RenderCards data={[]} title="No search results found" />
+                ) : (
+                  <RenderCards data={[]} title="No posts found" />
+                )}
+              </div>
             </>
           )}
         </div>
